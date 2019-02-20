83154eb776a60017b280f99e898667b0055ced16
Pranesh <ipraneshulleri@gmail.com>
2019-02-20 22:43:31 +0530

    Concept: Echo

---
+++ Concepts/echoFun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "just echo will print text "
+echo -n " \"echo -n\" will print the next echo "
+echo "<<like this one>>"
+echo -e " \"echo -e \" is used to remove backslash characters from the output "
+
