79d8d07b96d1c4dbbcec4983fe08f9a2abd6c786
Pranesh <ipraneshulleri@gmail.com>
2019-02-20 22:58:21 +0530

    Concept: comment lines

---
+++ Concepts/comment.sh
@@ -0,0 +1,10 @@
+#!bin/bash 
+: '
+here this text is commented
+and its multi lines .... noticed it is not its with apostrophe thats because 
+apostrophe is the end to multiline comment 
+'
+# single line comment here says hi to you !
+
+echo "check the script from a different level"
+
