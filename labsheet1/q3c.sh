97ac4508ceb73621078a8a4cc75238149e3f73b6
Pranesh <ipraneshulleri@gmail.com>
2019-02-21 02:19:42 +0530

    adding part c of q3

--- labsheet1/q3c.sh
+++ labsheet1/q3c.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo -n "Enter the number: "
-read n
-echo -n "Enter the limit: "
-read l
+filename="Q3c.txt"
+while read -r line; do
+    n="$line"
+done < $filename
+l=10
 counter=1
 while [ $counter -le $l ]
 	do
     a=`echo "$n * $counter" | bc -l`
 	echo "$n * $counter = $a"
-        ((counter++))
+    ((counter++))
     done
