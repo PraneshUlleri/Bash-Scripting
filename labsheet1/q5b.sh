3d3e5856ab68884777d6c6692ce7e74fdfb9bfb5
Pranesh <ipraneshulleri@gmail.com>
2019-02-25 23:22:27 +0530

    adding prime/non-prime number script part b

---
+++ labsheet1/q5b.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+number=$1
+i=2  
+f=0 
+while test $i -le `expr $number / 2`  
+do
+  
+if test `expr $number % $i` -eq 0  
+then
+f=1 
+fi
+   
+i=`expr $i + 1` 
+done
+if test $f -eq 1  
+then
+echo "Not a Prime Number"
+else
+echo "Prime Number"
+fi
