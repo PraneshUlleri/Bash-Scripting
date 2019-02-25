bcbf3d88c5384100833dc1644bcdc40511a33307
Pranesh <ipraneshulleri@gmail.com>
2019-02-25 23:16:15 +0530

    adding fibonaci script q4a.sh

---
+++ labsheet1/q4a.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo -n "Enter n :"
+read n
+counter=1
+if test $n -eq 1 
+then
+	echo "0"
+else
+first=0
+second=1
+echo -n "$first $second"
+((n=n-2))
+while [ $counter -le $n ]
+	do
+	third=`echo "$first + $second" | bc`
+	echo -n " $third"
+	first=$second;
+	second=$third;
+	((counter++))
+	done
+fi
