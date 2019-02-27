5a1933b8d556444e1d9a4b53ff604f39b60a88e7
Pranesh <ipraneshulleri@gmail.com>
2019-02-27 22:55:40 +0530

    labsheet 3 .1

---
+++ labsheet3/labsheet3-1.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+void  ChildProcess(void)
+{	
+	float side,perimeter,area;
+	printf("enter side: ");
+	scanf("%f",&side);
+	perimeter=4*side;	
+	area=side*side;
+	printf("Perimeter of square: %f",perimeter);
+	printf("\nArea of square: %f\n",area);
+}
+
+void  ParentProcess(void)
+{	
+	float radius,perimeter,area;
+	float pi=3.14; 
+	printf("enter radius: ");
+	scanf("%f",&radius);
+	perimeter=2*pi*radius;
+	area=pi*radius*radius; 
+	printf("circumference of circle: %f",perimeter);
+	printf("\narea of circle : %f\n",area);		
+}
+           
+int  main()
+{
+     pid_t pid;
+     pid=fork();
+     if (pid==0)
+          ChildProcess();
+     else 
+	{
+	  wait(NULL);
+          ParentProcess();
+	}
+      exit(0);
+}
+
