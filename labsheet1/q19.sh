e678bcf76816fe401a1bbe307101dbaa06111fb8
Pranesh <ipraneshulleri@gmail.com>
2019-03-04 02:15:24 +0530

    rename the file <low-upper>

---
+++ labsheet1/q19.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "keep this in the dir where you want to change !!!"
+echo " Lower to Upper : u"
+echo " Upper to Lower : l"
+read c
+case $c in 
+	u):
+	       rename 'y/a-z/A-Z/' *	
+		;;
+	l):
+		rename 'y/A-Z/a-z/' *
+		;;
+
+	esac
+ls
